ec5bb5acf82a47cc1fbca6b129c6a483d9fbf08f
Haiwei Su <haiweisu@Haiweis-MacBook-Pro.local>
2017-09-14 17:57:28 -0400

    creating comment UI

--- static/js/view/commentsView.js
+++ static/js/view/commentsView.js
@@ -2,10 +2,24 @@ define(function(require) {
   var Ember = require('ember');
 
   var T = Ember.Handlebars.compile('\
-    <div class="well"> \
-      This section should display a list of recent comments for the active post, and allow the user to \
-      delete a comment with the click of a button. \
-    </div> \
+    <span style="width: 90%"> \
+      {{textarea type="text" cols=100 placeholder="Write a comment" value=comment}} \
+    </span> \
+    <span style="width: 10%"> \
+      <button class="btn btn-primary pull-right" {{action submit comment controller.post_id}} >Comment</button> \
+    </span> \
+    <table class="table table-striped table-hover"> \
+      <thead><tr><th>Date</th><th>Message</th><th class="text-center">Delete Comment</th></thead> \
+      <tbody> \
+       {{#each comment in model}} \
+         <tr style="cursor: pointer;"> \
+           <td>{{date comment.created_time}}</td> \
+           <td>{{comment.message}}</td> \
+           <td class="text-center"><span {{action deleteComment comment.id}} class="delete-comment glyphicon glyphicon-trash"></span></td> \
+         </tr> \
+       {{/each}} \
+      </tbody> \
+    </table> \
   ');
 
   return Ember.View.extend({
