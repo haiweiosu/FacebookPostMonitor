31c6f1a38b6b8bc8205d6676ee958158fbaddcd7
Haiwei Su <haiweisu@Haiweis-MacBook-Pro.local>
2017-09-14 16:38:23 -0400

    calling model comment features function to create delete comment

--- static/js/controller/commentsController.js
+++ static/js/controller/commentsController.js
@@ -1,5 +1,29 @@
 define(function(require) {
-  var Ember = require('ember');
+  var Ember = require('ember'),
+  Q = require('Q'),
+  dctx = require('../model/datacontext');
 
-  return Ember.Controller;
+  return Ember.Controller.extend({
+  	comment: "",
+  	actions: {
+  		deleteComment: function(comment_id) {
+  			dctx.deleteComment(comment_id).then(
+  				function(r) {
+  					console.log(r);
+  				});
+  		},
+	  	submit: function(val, post_id) {
+	  		dctx.createComment(val, post_id).then(
+	  			function(r) {
+	  				console.log(r);
+	  			});
+	  	}
+  	},
+  	findComments: function(post_id) {
+  		return dctx.downloadComments(post_id).then(
+  			function(r) {
+  				return Q.resolve(Ember.getWithDefault(r, 'results', []));
+  			});
+  	}
+  });
 });
