99b86f9464638ad532392f20fb97531fe75e81f7
Haiwei Su <haiweisu@Haiweis-MacBook-Pro.local>
2017-09-14 17:48:57 -0400

    adding model for comment controller

--- static/js/route/commentsRoute.js
+++ static/js/route/commentsRoute.js
@@ -6,6 +6,12 @@ define(function(require) {
     view: require('../view/commentsView'),
     controller: require('../controller/commentsController'),
     route: Ember.Route.extend({
+      model: function(params, transition) {
+        var post = this.modelFor('post');
+        this.controllerFor('comments').post_id = post.id;
+ 
+         return this.controllerFor('comments').findComments(post.id);
+      },
       renderTemplate: function() {
         this.render({ into: 'post'});
       }
